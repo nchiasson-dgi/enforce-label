40299cc5ab8316f1b3d729e9cf163321c055638c
Noel Chiasson <noel@discoverygarden.ca>
2023-05-18 15:20:55 -0300

    Adding logic to restrict to only one required tag

--- dist/index.js
+++ dist/index.js
@@ -9657,9 +9657,15 @@ function run() {
         try {
             const labels = github.context.payload.pull_request.labels;
             const required_labels = (core.getInput('required_labels', { required: true }) !== '') ? (core.getInput('required_labels', { required: false })).split(',') : [];
+            // Check we have at least one of the required labels set.
             if (required_labels.length > 0 &&
                 !required_labels.some(requiredLabel => labels.find((l) => l.name === requiredLabel))) {
                 core.setFailed(`Please set at least one of the following labels on the PR: ${required_labels}`);
+            }
+            // Check to make sure we don't have more than one of the required labels set.
+            if (required_labels.length > 0 &&
+                !((labels.filter((label) => required_labels.find((l) => l === label.name))).length > 1)) {
+                core.setFailed(`Please set ONLY ONE of the following labels: ${required_labels}`);
             }
         }
         catch (error) {
